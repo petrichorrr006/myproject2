0d192034a24985dcbf9ef99b706b767a3715aad3
petrichorrr006 <raven24112006@gmail.com>
2026-02-09 01:59:08 +0500

    Update STATIC_ROOT path in settings.py for static file management

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,7 +125,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Media files (user-uploaded images)
